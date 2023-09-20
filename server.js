e3624d7bea5442a0cf406c792198dea205fad452
Peek Chan <exe985632@gmail.com>
2023-09-20 11:36:28 +0800

    updata

--- server.js
+++ server.js
@@ -15,36 +15,51 @@ app.use(cors());
 app.post("/api/logs", (req, res) => {
   if (!req.body) res.status(500).send("request body not found");
   const { robotId, data } = req.body;
-  const filename = `${robotId}_${moment
-    .utc()
-    .format("YYYY-MM-DD_HH-mm-ss")}_UTC`;
+  const filename = `${robotId}_${moment.utc().format("YYYY-MM-DD")}_UTC`;
 
   if (data.length <= 0)
     return res.status(500).json({ status: false, message: "empty array" });
-  fs.writeFile(
-    `./logs/${filename}.json`,
-    JSON.stringify(data, null, 2),
-    (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ status: false });
-      } else {
-        res.status(200).json({ status: true });
-      }
+
+  if (fs.existsSync(`./logs/${filename}.json`)) {
+    // const extractedObjects = data.flatMap(obj => obj);
+    // const jsonString = extractedObjects.map(obj => JSON.stringify(obj, null, 2)).join(",\n");
+
+    const origin = fs.readFileSync(`./logs/${filename}.json`, `utf8`);
+    const originJson = JSON.parse(origin);
+
+    const mergedData = originJson.concat(data);
+    try {
+      fs.writeFileSync(
+        `./logs/${filename}.json`,
+        JSON.stringify(mergedData, null, 2)
+      );
+    } catch (e) {
+      res.status(500).json({ status: false, message: e.message });
     }
-  );
+    res.status(200).json({ status: true });
+  } else {
+    fs.writeFile(
+      `./logs/${filename}.json`,
+      JSON.stringify(data, null, 2),
+      (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ status: false });
+        } else {
+          res.status(200).json({ status: true });
+        }
+      }
+    );
+  }
 });
 
-cron.schedule("*/30 * * * *", async () => {
+cron.schedule("*/30 * * * *", async () => {  // */30 * * * *
   const logs = await fs.promises.readdir("./logs");
   const currentDate = moment.utc();
   for (const file of logs) {
     const splitFilename = file.split("_");
-    const dataTimeString = splitFilename[1] + splitFilename[2];
-    const dataTimeUTC = moment
-      .utc(dataTimeString, "YYYY-MM-DD_HH-mm-ss")
-      .toDate();
-    const diffInDays = currentDate.diff(dataTimeUTC, "days");
+    const dataString = splitFilename[1];
+    const diffInDays = currentDate.diff(dataString, "days");
     if (diffInDays >= 30) {
       await fs.promises.unlink(`./logs/${file}`);
     }
@@ -63,7 +78,7 @@ function init() {
   }
 }
 
-app.listen(port, async() => {
+app.listen(port, async () => {
   console.log(`server starting on port ${port}`);
   await init();
 });
